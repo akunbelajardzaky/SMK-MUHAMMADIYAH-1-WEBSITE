7306b48d7e6611aba215d33b0f931135a2d4155f
mhm_dzaky <m.dzaky.abd123@gmail.com>
2023-05-29 14:54:00 +0700

    When you're done, just do the styling

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,5 +1,6 @@
 import { createClient, groq } from "next-sanity";
 import { Project } from "../types/project";
+import ClientConfig from "./config/client-config";
 
 export async function getProjects(): Promise<Project[]> {
   const client = createClient({
@@ -23,13 +24,7 @@ export async function getProjects(): Promise<Project[]> {
 }
 
 export async function getProject(slug: string): Promise<Project> {
-  const client = createClient({
-    projectId: "0kuojqyo",
-    dataset: "production",
-    apiVersion: "2023-05-28",
-  });
-
-  return client.fetch(
+  return createClient(ClientConfig).fetch(
     groq`*[_type == "project" && slug.current == $slug][0]{
     _id,
     _createdAt,
--- src/app/content/[content]/page.tsx
+++ src/app/content/[content]/page.tsx
@@ -1,4 +1,7 @@
+import { PortableTextInput } from "sanity";
 import { getProject } from "../../../../sanity/sanity-utils";
+import Image from "next/image";
+import { PortableText } from "@portabletext/react";
 
 type Props = {
   params: { content: string };
@@ -7,5 +10,30 @@ type Props = {
 export default async function Project({ params }: Props) {
   const slug = params.content;
   const project = await getProject(slug);
-  return <div>{project.name}</div>;
+  return (
+    <div>
+      <h1>{project.name}</h1>
+
+      <a
+        href={project.url}
+        title="Vieww"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        kalo di klik ngarah ke url
+      </a>
+      {project.image && (
+        <Image
+          src={project.image}
+          alt={project.name}
+          width={250}
+          height={100}
+          className="object-cover rounded-lg border border-gray-500"
+        />
+      )}
+      <div>
+        <PortableText value={project.content} />
+      </div>
+    </div>
+  );
 }
