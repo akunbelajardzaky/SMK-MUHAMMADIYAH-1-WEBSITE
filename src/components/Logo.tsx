a3d0963f527e24838cc0513268b84bef36160c97
mhm_dzaky <m.dzaky.abd123@gmail.com>
2023-05-29 20:20:45 +0700

    navbars done

--- src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Image from "next/image";
-import Ikj from "../assets/icon/ikj.svg";
-
-export default function Logo() {
-  return (
-    <Image src={Ikj} width={87} height={87} alt="Info kajian jogjakarta" />
-  );
-}
