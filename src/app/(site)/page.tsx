9ad35f52e66eb1b04df1107fec8e64980bc21c3a
mhm_dzaky <m.dzaky.abd123@gmail.com>
2023-05-30 14:09:52 +0700

    just change the folder to make it tidier

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,26 +1,8 @@
 import React from "react";
 import { HeaderAction } from "../../components/Navbar";
+import links from "../../../types/links";
 
 function Page() {
-  const links = [
-    {
-      link: "link1",
-      label: "Link 1",
-      links: [
-        { link: "link1-1", label: "Link 1-1" },
-        { link: "link1-2", label: "Link 1-2" },
-      ],
-    },
-    {
-      link: "link2",
-      label: "Link 2",
-      links: [
-        { link: "link2-1", label: "Link 2-1" },
-        { link: "link2-2", label: "Link 2-2" },
-      ],
-    },
-  ];
-
   return (
     <>
       <div className="mt-10">
